0227060e92903950dca7708c5caa0caf215ae97b
Sunny <sunnyinvadeher@gmail.com>
2018-10-26 22:06:39 -0700

    Insertion + Binary Search

--- doalg.c
+++ doalg.c
@@ -2,68 +2,44 @@
 
 int doalg(int n, int k, int* Best){
     int arr[10001]={};
-    int heap[41]={};
-    int h_cnt = 0;
-    for(int i=1;i<=n;i++) arr[i]=i;
-    for(int i=1;i<=n;i++) {
-        if(!h_cnt)        
-            heap[h_cnt++] = arr[i];
-        else {
-            int cur_loc = h_cnt;
-            // 接40th之前
-            if(h_cnt >= 40 && COMPARE(heap[0], arr[i])==1){
-                continue;
+    int sort[40]={};
+    for(int i=0;i<=n;i++) 
+        arr[i]=i;
+    // Use insertion sort for first 40 elements
+    sort[0] = arr[1];
+    for(int i=2;i<=40;i++) {
+        int j=i-2;
+        // Order it from high value to low value
+        // arr[i]=key > sort[j], move it ahead.
+        while(j>=0 && COMPARE(arr[i],sort[j])==1) {
+            sort[j+1] = sort[j];
+            j--;
+        }
+        sort[j+1] = arr[i];
+    }
+
+    // Now we got a sorted array, so we're able to use binary search to insert new elements
+    for(int i=41;i<=n;i++) {
+        int l=0,r=40;
+        while(l<r) {
+            int mid = l+(r-l)/2;
+            if(COMPARE(arr[i],sort[mid])==1) {
+                r=mid;
             } else {
-                heap[h_cnt] = arr[i];
-                while(COMPARE(heap[cur_loc], heap[(cur_loc-1)/2])==2){
-                    cur_loc = (cur_loc-1)/2;
-                    heap[h_cnt] = heap[h_cnt]^heap[(h_cnt-1)/2];
-                    heap[(h_cnt-1)/2] = heap[h_cnt]^heap[(h_cnt-1)/2];
-                    heap[h_cnt] = heap[h_cnt]^heap[(h_cnt-1)/2];
-                }
-                h_cnt = (h_cnt < 40) ? h_cnt+1 : 40;
+                l=mid+1;
             }
         }
-        if( h_cnt >= 40) {
-            heap[0] = arr[i];
-            int cur = 0;
-            int l = 1;
-            int r = 2;
-            while(l <= 40 && r <= 40) {
-                // printf("i=%d, cur=%d\n",i,cur);
-                l = cur*2+1;
-                r = cur*2+2;
-                
-                if((COMPARE(heap[cur],heap[l])==2) && (COMPARE(heap[cur], heap[r])==2))
-                    break;
-                if(COMPARE(heap[l], heap[r])==1) {
-                    // 走右邊
-                    if(COMPARE(heap[cur], heap[r])==1) {
-                        heap[cur] = heap[cur]^heap[r];
-                        heap[r] = heap[cur]^heap[r];
-                        heap[cur] = heap[cur]^heap[r];
-                        cur = r;
-                    }
-                } else {
-                    if(COMPARE(heap[cur], heap[l])==1) {
-                        heap[cur] = heap[cur]^heap[l];
-                        heap[l] = heap[cur]^heap[l];
-                        heap[cur] = heap[cur]^heap[l];
-                        cur = l;
-                    }
-                }
-                
-                // printf("HEAPIFY!\n");
-            } 
+        // Insert arr[i] into sort[l], but we have to move each element after "l"
+        int k = 38;
+        while(k >= l) {
+            sort[k+1] = sort[k];
+            k--;
         }
+        sort[l] = arr[i];
     }
-    
+
     for(int i=0;i<40;i++) {
-        printf("%d\n", heap[i]);
-    }
-    int cnt = 0;
-    for(int i=n;i>=n-k;i--) {
-        Best[cnt++] = arr[i];
+        Best[i] = sort[i];
     }
     return 1;
 }
--- main.c
+++ main.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #define MAXN 10000
 #define MAXK 100
-#include "compare.c"
+#include "COMPARE.c"
 #include "doalg.c"
-
 main(){
-	int N = 1000;
+	int N = 10000;
 	int K = 40;
 	int Best[MAXK];
 	int k,n,flag,round,ct,loop,biggest,total;
 
 	total = 0;
 	biggest = -1;
-	for (loop=0; loop<1; loop++) {
+	for (loop=0; loop<1000; loop++) {
 		n = N;
 		k = K;
 		COMPARE(0,n);
-		flag = doalg(n,k,Best);
+		flag = doalg( n, k, Best);
 		if (flag==0) {
 			printf(" *** flagged error at loop=%d\n",loop);
 			return(0);
 		}
-
 		ct = COMPARE(-1,k,Best);
 		if (ct<=0) {
 			printf(" *** error at loop=%d\n",loop);
