60f6e37d029c96294737f05a0e4b32fd6333431a
Sunny <sunnyinvadeher@gmail.com>
2018-10-27 00:06:45 -0700

    Removed redundant code

--- doalg.c
+++ doalg.c
@@ -3,25 +3,26 @@
 int doalg(int n, int k, int* Best){
     int arr[10001]={};
     int sort[40]={};
-    for(int i=0;i<=n;i++) arr[i]=i;
-    sort[0]=arr[1];
+    sort[0]=1;
 
     for(int i=2;i<=n;i++) {
-        int l=0, r=(i>40)?40:i-1;
+        int l=0, r=(i>k)?k:i-1;
+        if(i>k && COMPARE(i,sort[k-1])==2)
+            continue;
         while(l<r) {
             int mid=l+(r-l)/2;
-            if(COMPARE(arr[i],sort[mid])==1) {
+            if(COMPARE(i,sort[mid])==1) {
                 r=mid;
             } else {
                 l=mid+1;
             }
         }
-        int k=(i>40)?38:i-2;
-        while(k>=l) {
-            sort[k+1]=sort[k];
-            k--;
+        int j=(i>k)?k-2:i-2;
+        while(j>=l) {
+            sort[j+1]=sort[j];
+            j--;
         }
-        sort[l]=arr[i];
+        sort[l]=i;
     }
 
     for(int i=0;i<40;i++) {
